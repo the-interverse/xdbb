86af6ef3fe5eb44f38d0f9c84583ba5cd3f24f00
geometry-dictionary <36106744+geometry-dictionary@users.noreply.github.com>
2018-05-13 20:22:11 -0400

    Update validation.test.js

--- server/utils/validation.test.js
+++ server/utils/validation.test.js
@@ -4,7 +4,7 @@ const {isRealString} = require('./validation.js');
 
 describe('isRealString', () => {
 	it('should reject non string values', () => {
-		var res = isRealString(98);
+		var res = isRealString();
 		expect(res).toBe(false);
 	});
 	it('should reject string with only spaces', () => {
